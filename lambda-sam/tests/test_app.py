0926aa0ad18d2bab10874f65db088d8e37aeab27
SUBHASIS SAMAL <11335722+subhasam@users.noreply.github.com>
2025-09-20 10:48:59 -0700

    (fix) : Unit test failure

--- lambda-sam/tests/test_app.py
+++ lambda-sam/tests/test_app.py
@@ -7,4 +7,4 @@ def test_lambda_handler():
     response = lambda_handler(event, context)
     assert response["statusCode"] == 200
     body = json.loads(response["body"])
-    assert body["message"] == "Hello, DISCOVER!"
+    assert body["message"] == "Hello!! We offer supports for all Digital Wallets and Payment Methods."
